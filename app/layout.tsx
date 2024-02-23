04be532d24987ff502e492b9e97fea9436fed3e7
Yogesh Shahi <yogesh@aeriuslabs.org>
2024-02-23 18:54:52 +0530

    feat(wip): rainbowkit and sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,23 @@
 'use client';
+
 import { Inter } from 'next/font/google';
 import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 import { PrivyProvider } from '@privy-io/react-auth';
 import { useRouter } from 'next/navigation';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, base, zora } from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base, zora],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -14,34 +28,40 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <PrivyProvider
-          appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
-          onSuccess={() => router.push('/token-purchase')}
-          config={{
-            appearance: {
-              accentColor: '#6A6FF5',
-              theme: '#FFFFFF',
-              showWalletLoginFirst: false,
-              logo: '/plume-logo.svg',
-            },
-            loginMethods: [
-              'wallet',
-              'google',
-              'discord',
-              'github',
-              'email',
-              'twitter',
-              'apple',
-            ],
-            embeddedWallets: {
-              createOnLogin: 'users-without-wallets',
-              requireUserPasswordOnCreate: false,
-            },
-            mfa: { noPromptOnMfaRequired: false },
-          }}
-        >
-          {children}
-        </PrivyProvider>
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <RainbowKitProvider>
+              <PrivyProvider
+                appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
+                onSuccess={() => router.push('/token-purchase')}
+                config={{
+                  appearance: {
+                    accentColor: '#6A6FF5',
+                    theme: '#FFFFFF',
+                    showWalletLoginFirst: false,
+                    logo: '/plume-logo.svg',
+                  },
+                  loginMethods: [
+                    'wallet',
+                    'google',
+                    'discord',
+                    'github',
+                    'email',
+                    'twitter',
+                    'apple',
+                  ],
+                  embeddedWallets: {
+                    createOnLogin: 'users-without-wallets',
+                    requireUserPasswordOnCreate: false,
+                  },
+                  mfa: { noPromptOnMfaRequired: false },
+                }}
+              >
+                {children}
+              </PrivyProvider>
+            </RainbowKitProvider>
+          </QueryClientProvider>
+        </WagmiProvider>
       </body>
     </html>
   );
