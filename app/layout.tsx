3a757b92f17fe128b34b689f034bbbfda715fab6
Yogesh Shahi <65803503+BornPsych@users.noreply.github.com>
2024-02-23 18:51:00 +0530

    [ENG-35] feat: privy integration (#2)
    feat: privy integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,48 @@
-import type { Metadata } from 'next';
+'use client';
 import { Inter } from 'next/font/google';
 import './globals.css';
-
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Asset Buyer',
-  description: 'Asset buyer by plume network',
-};
+import { PrivyProvider } from '@privy-io/react-auth';
+import { useRouter } from 'next/navigation';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <PrivyProvider
+          appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
+          onSuccess={() => router.push('/token-purchase')}
+          config={{
+            appearance: {
+              accentColor: '#6A6FF5',
+              theme: '#FFFFFF',
+              showWalletLoginFirst: false,
+              logo: '/plume-logo.svg',
+            },
+            loginMethods: [
+              'wallet',
+              'google',
+              'discord',
+              'github',
+              'email',
+              'twitter',
+              'apple',
+            ],
+            embeddedWallets: {
+              createOnLogin: 'users-without-wallets',
+              requireUserPasswordOnCreate: false,
+            },
+            mfa: { noPromptOnMfaRequired: false },
+          }}
+        >
+          {children}
+        </PrivyProvider>
+      </body>
     </html>
   );
 }
---
+++ app/token-purchase/page.tsx
@@ -0,0 +1,3 @@
+export default function TokenPurchase() {
+  return <h1>Token Purchase</h1>;
+}
