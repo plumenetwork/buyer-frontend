86e6711f231c1a15a2ee8a64fc771e1e05465e0b
Eugene Y. Q. Shen <eugeneyqshen@gmail.com>
2024-03-13 23:39:56 -0400

    add https://assets.plumenetwork.xyz as an allowed remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,26 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "assets.plumenetwork.xyz",
+        }
+      ]
+    },
     webpack: (config, { isServer }) => {
       if (!isServer) {
         // Provide empty mocks for Node.js modules that are not supported in the browser
         config.resolve.fallback = {
           ...config.resolve.fallback,
           fs: false,
-          net:false,
+          net: false,
           tls: false,
         };
       }
       return config;
     },
   };
-  
+
 export default nextConfig;
