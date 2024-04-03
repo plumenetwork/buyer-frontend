3f8481ce4235f44ba4552b722c77d3149d24a4c2
Yogesh Shahi <yogesh@aeriuslabs.org>
2024-04-03 16:37:48 +0530

    chore: revised breakpoint to avoid overlap

--- app/token-purchase/page.tsx
+++ app/token-purchase/page.tsx
@@ -47,7 +47,7 @@ function TokenPurchase() {
           <div
             className={`flex h-screen w-screen items-center justify-center ${inter.className}`}
           >
-            <div className='hidden h-full w-1/3 md:flex'>
+            <div className='hidden h-full w-1/3 lg:flex'>
               <StepperBar tabs={tabs} />
             </div>
             <div className='flex h-full w-2/3 items-center justify-center'>
